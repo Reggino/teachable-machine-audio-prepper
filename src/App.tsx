77c243bdb3eec2404fd9d2e168e8ea8d5b24fea1
Tim de Koning <git@king-management.nl>
2022-12-28 00:30:02 +0100

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import JSZip from "jszip";
+import FileSaver from "file-saver";
 
 function App() {
+  const onChange = React.useCallback((e: any) => {
+    const reader = new FileReader();
+    reader.addEventListener("load", async (event) => {
+      if (!event.target?.result || typeof event.target?.result === "string") {
+        throw new Error("Missing result from FileReader");
+      }
+      let c = new AudioContext({
+        sampleRate: 44100,
+      });
+      let b = await c.decodeAudioData(event.target.result);
+      let freqDataQueue: any = [];
+      let columnTruncateLength = 232;
+      let sampleRate = 44100;
+
+      let oac = new OfflineAudioContext({
+        numberOfChannels: b.numberOfChannels,
+        length: b.length,
+        sampleRate: sampleRate,
+      });
+
+      const source = oac.createBufferSource();
+      const processor = oac.createScriptProcessor(1024, 1, 1);
+
+      const analyser = oac.createAnalyser();
+      analyser.fftSize = 2048;
+      analyser.smoothingTimeConstant = 0;
+
+      source.buffer = b;
+
+      source.connect(analyser);
+      analyser.connect(processor);
+      processor.connect(oac.destination);
+
+      var freqData = new Float32Array(analyser.fftSize);
+      processor.onaudioprocess = () => {
+        analyser.getFloatFrequencyData(freqData);
+        freqDataQueue.push(freqData.slice(0, columnTruncateLength));
+      };
+
+      source.start(0);
+      oac.startRendering();
+
+      oac.oncomplete = (e) => {
+        const zip = new JSZip();
+        zip.file(
+          "sample.json",
+          JSON.stringify([
+            {
+              frequencyFrames: freqDataQueue,
+              blob: null,
+              startTime: 0,
+              endTime: b.duration,
+              recordingDuration: b.duration,
+              blobFilePath: "sample.webm",
+            },
+          ])
+        );
+        // zip.file(event.target.result);
+        zip.generateAsync({ type: "blob" }).then((content) => {
+          FileSaver.saveAs(content, "samples.zip");
+        });
+
+        source.disconnect(analyser);
+        processor.disconnect(oac.destination);
+      };
+    });
+    reader.readAsArrayBuffer(e.target.files[0]);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" id="input" multiple onChange={onChange} />
     </div>
   );
 }
